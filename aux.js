35b5beb8a832de27e070012ca87a080ae55d8274
DecaHub <dan@decahub.com>
2017-03-08 15:10:17 -0500

    Create module for auxiliary function: isString, isArray

---
+++ aux.js
@@ -0,0 +1,24 @@
+/**
+ * Created by DecaHub on 3/8/17.
+ */
+
+"use strict";
+
+const isString = function (obj) {
+	
+	return typeof obj.target !== "string" || obj.target instanceof String
+	
+};
+
+const isArray = function (obj) {
+	
+	return Array.isArray(obj);
+	
+};
+
+module.exports = {
+	
+	isString,
+	isArray
+	
+};
