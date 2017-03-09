abaf743e24c5d9bdcfb899a5dc3366b1ed62e9d9
DecaHub <dan@decahub.com>
2017-03-09 11:53:12 -0500

    Create FInderTask class module to define the object that is used by ngfinder module to search for angular files

---
+++ finderTask.js
@@ -0,0 +1,20 @@
+/**
+ * Created by DecaHub on 3/8/17.
+ */
+
+"use strict";
+
+class FinderTask {
+	
+	constructor () {
+		
+		this.props = new Set();
+		
+		this.props.add("target");
+		this.props.add("ignore");
+		
+	}
+	
+}
+
+module.exports = new FinderTask();
