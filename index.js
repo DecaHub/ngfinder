9697ba0591431fdac6f1bf346b744584919b96e7
DecaHub <dan@decahub.com>
2017-03-09 12:10:37 -0500

    Modularize NG asset management, error handling, FinderTask object.
    Better testing!

--- index.js
+++ index.js
@@ -10,54 +10,14 @@ const fs = require("fs");
 const errHandler = require("./errorHandler");
 const aux = require("./aux");
 
-let root = "";
-const rootFiles = new Set();
+const log = require("bootstrap-logs");
 
-const types = {
-	app: ".app",
-	module: ".module",
-	constants: ".constants",
-	provider: ".provider",
-	enum: ".enum",
-	model: ".model",
-	config: ".config",
-	filter: ".filter",
-	directive: ".directive",
-	decorator: ".decorator",
-	interceptor: ".interceptor",
-	service: ".service",
-	workflow: ".workflow",
-	repository: ".repository",
-	resolver: ".resolver",
-	controller: ".controller",
-	component: ".component"
-};
-
-const ng = new Map();
-
-ng.set("Apps", new Set());
-ng.set("Modules", new Set());
-ng.set("Constants", new Set());
-ng.set("Providers", new Set());
-ng.set("Enums", new Set());
-ng.set("Models", new Set());
-ng.set("Configs", new Set());
-ng.set("Filters", new Set());
-ng.set("Directives", new Set());
-ng.set("Decorators", new Set());
-ng.set("Interceptors", new Set());
-ng.set("Services", new Set());
-ng.set("Workflows", new Set());
-ng.set("Repositories", new Set());
-ng.set("Resolvers", new Set());
-ng.set("Controllers", new Set());
-ng.set("Components", new Set());
-ng.set("OtherJS", new Set());
-
-const finderTaskProps = new Set();
-
-finderTaskProps.add("target");
-finderTaskProps.add("ignore");
+log.custom({
+	danger : "[  ERROR  ] ",
+	info   : "[  INFO   ] ",
+	warning: "[ WARNING ] ",
+	success: "[ SUCCESS ] "
+});
 
 const fileWalker = function (dir, files, ignorePaths) {
 	
@@ -85,91 +45,25 @@ const fileWalker = function (dir, files, ignorePaths) {
 			
 		});
 		
+		return files;
+		
 	} catch (error) {
 		
 		if (error.code === "ENOENT") {
 			
-			console.error(`Specified Target Directory not found: ${error.path}`);
+			log.danger(`Specified Target Directory not found: ${error.path}`);
 			
-		} else {
-			
-			throw error;
+			return null;
 			
 		}
 		
-	}
-	
-};
-
-const filterFile = function (item) {
-	
-	const sansJS = path.basename(item, ".js");
-	const type = path.extname(sansJS);
-	
-	switch (type) {
-		
-		case types.app:
-			ng.get("Apps").add(item);
-			break;
-		case types.module:
-			ng.get("Modules").add(item);
-			break;
-		case types.constants:
-			ng.get("Constants").add(item);
-			break;
-		case types.provider:
-			ng.get("Providers").add(item);
-			break;
-		case types.enum:
-			ng.get("Enums").add(item);
-			break;
-		case types.model:
-			ng.get("Models").add(item);
-			break;
-		case types.config:
-			ng.get("Configs").add(item);
-			break;
-		case types.filter:
-			ng.get("Filters").add(item);
-			break;
-		case types.directive:
-			ng.get("Directives").add(item);
-			break;
-		case types.decorator:
-			ng.get("Decorators").add(item);
-			break;
-		case types.interceptor:
-			ng.get("Interceptors").add(item);
-			break;
-		case types.service:
-			ng.get("Services").add(item);
-			break;
-		case types.workflow:
-			ng.get("Workflows").add(item);
-			break;
-		case types.repository:
-			ng.get("Repositories").add(item);
-			break;
-		case types.resolver:
-			ng.get("Resolvers").add(item);
-			break;
-		case types.controller:
-			ng.get("Controllers").add(item);
-			break;
-		case types.component:
-			ng.get("Components").add(item);
-			break;
-		default:
-			ng.get("OtherJS").add(item);
-			break;
+		throw error;
 		
 	}
 	
 };
 
 const getPath = function (string) {
-	
-	console.log(`getPath: ${string}`);
 	
 	let rootPath = "";
 	
@@ -179,65 +73,57 @@ const getPath = function (string) {
 		
 		return rootPath;
 		
-	} else {
+	}
+	
+	const temp = string.split(/[\\/]/);
+	
+	for (let elem = 0; elem < temp.length; elem++) {
 		
-		const temp = string.split(/[\\/]/);
-		
-		for (let elem = 0; elem < temp.length; elem++) {
+		if (temp[elem] === "" || temp[elem] === null) {
 			
-			if (temp[elem] === "" || temp[elem] === null) {
-				
-				temp.splice(elem, 1);
-				
-				/**
-				 * Rewind elem by 1;
-				 * else elem next to the spliced elem won't be inspected.
-				 */
-				
-				elem -= 1;
-				
-			} else if (elem < temp.length - 1) {
-				
-				rootPath += `${temp[elem]}/`;
-				
-			} else {
-				
-				rootPath += temp[elem];
-				
-			}
+			temp.splice(elem, 1);
+			
+			/**
+			 * Rewind elem by 1;
+			 * else elem next to the spliced elem won't be inspected.
+			 */
+			
+			elem -= 1;
+			
+		} else if (elem < temp.length - 1) {
+			
+			rootPath += `${temp[elem]}/`;
+			
+		} else {
+			
+			rootPath += temp[elem];
 			
 		}
 		
-		return rootPath;
-		
 	}
+	
+	return rootPath;
 	
 };
 
 const processIgnorePaths = function (_ignore) {
 	
-	console.log(_ignore);
-	
 	const ignoreSet = new Set();
+	let stringArray = [];
 	
-	if (aux.isArray(_ignore)) {
+	if (aux.isString(_ignore)) {
 		
+		stringArray.push(_ignore);
 		
+	} else if (aux.isArray(_ignore)) {
 		
-	} else if (aux.isString(_ignore)) {
-		
-		let temp = _ignore;
-		_ignore = [];
-		
-		_ignore.push(temp);
+		stringArray = _ignore;
 		
 	}
 	
-	for (let elem = 0; elem < _ignore.length; ++elem) {
+	for (let elem = 0; elem < stringArray.length; ++elem) {
 		
-		const tempPath = getPath(_ignore[elem]);
-		
-		console.log(`processIgnorePaths: ${tempPath}`);
+		const tempPath = getPath(stringArray[elem]);
 		
 		if (tempPath) {
 			
@@ -253,55 +139,81 @@ const processIgnorePaths = function (_ignore) {
 
 const ngFinder = function (finderTask) {
 	
+	const ng = new (require("./ng"))();
+	const validFinderTask = require("./finderTask");
+	
+	let root = null;
+	const rootFiles = new Set();
+	
+	let ignorePaths = null;
+	const ngFiles = [];
+
+	/**
+	 * Check object shell integrity
+	 */
+
 	try {
-		
+
 		errHandler.isInvalidObj(finderTask);
-		errHandler.hasUnknownProperties(finderTask, finderTaskProps);
-		errHandler.hasAllRequiredProps(finderTask, finderTaskProps);
+		errHandler.hasUnknownProperties(finderTask, validFinderTask.props);
+		errHandler.hasAllRequiredProps(finderTask, validFinderTask.props);
+
+	} catch (error) {
+
+		log.danger(error.message);
 		
+		return null;
+
 	}
-	catch (error) {
+
+	/**
+	 * Check property integrity
+	 */
+
+	try {
+
+		errHandler.isTargetValid(finderTask.target);
+		errHandler.isIgnoreValid(finderTask.ignore);
+
+	} catch (error) {
+
+		log.danger(error.message);
 		
-		console.log(error.message);
+		return null;
+
+	}
+
+	root = getPath(finderTask.target);
+	ignorePaths = processIgnorePaths(finderTask.ignore);
+
+	const walker = fileWalker(root, rootFiles, ignorePaths);
+	
+	if (!walker) {
+		
 		return null;
 		
 	}
-	
-	let ignorePaths;
-	const ngFiles = [];
-	
-	root = getPath(finderTask.target);
-	
-	
-	
-	ignorePaths = processIgnorePaths(finderTask.ignore);
-	
-	console.log(`ngFinder.ignorePaths:`);
-	console.log(ignorePaths);
-	
-	
-	fileWalker(root, rootFiles, ignorePaths);
-	
+
 	for (const item of rootFiles) {
-		
-		filterFile(item);
-		
+
+		ng.filterFile(item);
+
 	}
-	
-	for (const [name, set] of ng.entries()) {
-		
+
+	for (const [name, set] of ng.map.entries()) {
+
 		if (set.size > 0) {
-			
+
 			for (const file of set) {
-				
+
 				ngFiles.push(file);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	return ngFiles;
 	
 };
