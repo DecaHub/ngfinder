f97a76fa4b0d7deff91e8182b33897607ab6666a
DecaHub <dan@decahub.com>
2017-03-09 20:03:00 -0500

    Differentiate between required and optional properties using sets for fast search.

--- finderTask.js
+++ finderTask.js
@@ -9,9 +9,31 @@ class FinderTask {
 	constructor () {
 		
 		this.props = new Set();
+		this.requiredProps = new Set();
+		this.optionalProps = new Set();
 		
-		this.props.add("target");
-		this.props.add("ignore");
+		this.props.add({
+			name: "target",
+			required: true
+		});
+		this.props.add({
+			name: "ignore",
+			required: false
+		});
+		
+		for (const prop of this.props) {
+			
+			if (prop.required) {
+				
+				this.requiredProps.add(prop.name);
+				
+			} else {
+				
+				this.optionalProps.add(prop.name);
+				
+			}
+			
+		}
 		
 	}
 	
